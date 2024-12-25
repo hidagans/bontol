00635063b15d07d7a3b48a8be8ba713c56ede31b
hidagans <58388773+hidagans@users.noreply.github.com>
2024-12-26 00:49:30 +0700

    Update jaseb_db.py

--- ubot/core/database/jaseb_db.py
+++ ubot/core/database/jaseb_db.py
@@ -1,19 +1,19 @@
 from ubot.core.database import mongodb
 
 async def get_jaseb_text():
-    return await db.get("jaseb_text")
+    return await mongodb.get("jaseb_text")
 
 async def set_jaseb_text(text):
-    await db.set("jaseb_text", text)
+    await mongodb.set("jaseb_text", text)
 
 async def get_jaseb_interval():
-    return await db.get("jaseb_interval")
+    return await mongodb.get("jaseb_interval")
 
 async def set_jaseb_interval(interval):
-    await db.set("jaseb_interval", interval)
+    await mongodb.set("jaseb_interval", interval)
 
 async def get_jaseb_target():
-    return await db.get("jaseb_target")
+    return await mongodb.get("jaseb_target")
 
 async def set_jaseb_target(target):
-    await db.set("jaseb_target", target)
+    await mongodb.set("jaseb_target", target)
