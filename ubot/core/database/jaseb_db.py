39c24d61b5a0ccb1cf71a35b532cfd89e3706b4c
hidagans <58388773+hidagans@users.noreply.github.com>
2024-12-26 00:52:45 +0700

    Update jaseb_db.py

--- ubot/core/database/jaseb_db.py
+++ ubot/core/database/jaseb_db.py
@@ -3,19 +3,22 @@ from ubot.core.database import mongodb
 jaseb = mongodb["ubot"]["jaseb"]
 
 async def get_jaseb_text():
-    return await jaseb.get("jaseb_text")
+    result = await jaseb.find_one({"_id": "jaseb_text"})
+    return result["value"] if result else None
 
 async def set_jaseb_text(text):
-    await jaseb.set("jaseb_text", text)
+    await jaseb.update_one({"_id": "jaseb_text"}, {"$set": {"value": text}}, upsert=True)
 
 async def get_jaseb_interval():
-    return await jaseb.get("jaseb_interval")
+    result = await jaseb.find_one({"_id": "jaseb_interval"})
+    return result["value"] if result else None
 
 async def set_jaseb_interval(interval):
-    await jaseb.set("jaseb_interval", interval)
+    await jaseb.update_one({"_id": "jaseb_interval"}, {"$set": {"value": interval}}, upsert=True)
 
 async def get_jaseb_target():
-    return await jaseb.get("jaseb_target")
+    result = await jaseb.find_one({"_id": "jaseb_target"})
+    return result["value"] if result else None
 
 async def set_jaseb_target(target):
-    await jaseb.set("jaseb_target", target)
+    await jaseb.update_one({"_id": "jaseb_target"}, {"$set": {"value": target}}, upsert=True)
